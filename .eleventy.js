1dcc1e5fa7c84864f152100208c533038aa3df90
Kimser91 <149468829+Kimser91@users.noreply.github.com>
2025-08-25 14:31:48 +0200

    bug fix

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/admin");
+  eleventyConfig.addPassthroughCopy("src/images");
+
   eleventyConfig.addCollection("blog", function(collectionApi) {
     return collectionApi.getFilteredByTag("blog");
   });
