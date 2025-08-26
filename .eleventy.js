0f4b1e79afd95d2ce89df8c896f58113ef3e64b9
Kimser91 <149468829+Kimser91@users.noreply.github.com>
2025-08-26 08:08:56 +0200

    decap bugs

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,6 @@
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/admin");
   eleventyConfig.addPassthroughCopy("src/images");
-
-  eleventyConfig.addCollection("blog", function(collectionApi) {
-    return collectionApi.getFilteredByTag("blog");
-  });
-
-  return {
-    dir: {
-      input: "src",
-      includes: "_includes",
-      output: "dist"
-    }
-  };
+  eleventyConfig.addCollection("blog", c => c.getFilteredByTag("blog"));
+  return { dir: { input: "src", includes: "_includes", output: "dist" } };
 };
